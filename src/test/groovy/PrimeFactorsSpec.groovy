545eba7ef796c6a85d3159cddfcc7a7050f09dd0
Juan Lladó <jllado@gmail.com>
2017-07-02 17:11:39 +0200

    First commit

---
+++ src/test/groovy/PrimeFactorsSpec.groovy
@@ -0,0 +1,18 @@
+import spock.lang.Specification
+
+
+/**
+ * 1  -> []
+ * 2  -> [2]
+ * 3  -> [3]
+ * 4  -> [2, 2]
+ * 5  -> [5]
+ * 6  -> [2, 3]
+ * 7  -> [7]
+ * 8  -> [2, 2, 2]
+ * 9  -> [3]
+ * 10 -> [2, 5]
+ */
+class PrimeFactorsSpec extends Specification {
+
+}
