edb454db65d47cf77d751199a10b9cb6c960db32
Juan Lladó <jllado@gmail.com>
2017-07-02 18:51:11 +0200

    Remaining test cases

--- src/test/groovy/PrimeFactorsSpec.groovy
+++ src/test/groovy/PrimeFactorsSpec.groovy
@@ -1,12 +1,6 @@
 import spock.lang.Specification
 
 
-/**
- * 7  -> [7]
- * 8  -> [2, 2, 2]
- * 9  -> [3]
- * 10 -> [2, 5]
- */
 class PrimeFactorsSpec extends Specification {
 
     def "return prime factors of a integer"() {
@@ -20,6 +14,10 @@ class PrimeFactorsSpec extends Specification {
         4     | [2, 2]
         5     | [5]
         6     | [2, 3]
+        7     | [7]
+        8     | [2, 2, 2]
+        9     | [3, 3]
+        10    | [2, 5]
     }
 
     def primeFactors(int input) {
